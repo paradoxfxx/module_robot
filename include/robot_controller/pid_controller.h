da740c2c7dec9727f0de2af4ba2120024538290e
Jiongyu <35024339@qq.com>
2019-07-03 22:46:23 +0800

    add pid controller

--- include/robot_controller/pid_controller.h
+++ include/robot_controller/pid_controller.h
@@ -1,56 +1,97 @@
 #ifndef PID_CONTROLLER_H_
 #define PID_CONTROLLER_H_
 
-#include <stdlib.h>
-#include <math.h>
+#include <stdbool.h>
+#include <stdint.h>
 
 namespace controller
 {
 
+
+/**
+ * Defines if the controler is direct or reverse
+ */
+enum pid_control_directions {
+	E_PID_DIRECT,
+	E_PID_REVERSE,
+};
+
+/**
+ * Structure that holds PID all the PID controller data, multiple instances are
+ * posible using different structures for each controller
+ */
+struct pid_controller {
+	
+};
+
+typedef struct pid_controller * pid_t;
+
 class PID_controller
 {
-    
-public:
-    /** Initializes a PID controller. */
-    PID_controller();
+	float last_error;
+        float last_output;
+        float last_last_error;
+        float set_point;
+        float Kp, Ki, Kd;
+        float integral;
+        float interval;
+        float error_threshold;
+        float output_upper_limit;
+        float output_lower_limit;
+        float error;
 
+        
+        public:
+            /// Public constructor
+            PID_controller();
 
-    /** Sets the gains of the given PID. */
-    void pid_set_gains(float kp, float ki, float kd);
+            /// Set the proportional term. Use it AFTER setRefreshInterval or setRefreshRate
+            void setKp(const float &Kp);
 
-    /** Returns the proportional gains of the controller. */
-    void pid_get_gains(float &kp, float &ki, float &kd);
+            /// Set the integral term. Use it AFTER setRefreshInterval or setRefreshRate
+            void setKi(const float &Ki);
 
-    /** Returns the limit of the PID integrator. */
-    float pid_get_integral_limit();
+            /// Set the derivative term. Use it AFTER setRefreshInterval or setRefreshRate
+            void setKd(const float &Kd);
 
-    /** Returns the value of the PID integrator. */
-    float pid_get_integral();
+            /// Set the P, I, D terms respectively. Use it AFTER setRefreshInterval or setRefreshRate
+            void setWeights(const float &Kp, const float &Ki, const float &Kd);
 
-    /** Process one step if the PID algorithm. */
-    float pid_process(float error);
+            /// Set the refresh interval of the controller in seconds.
+            /// If you don't know the interval you can set the frequency of update using setRefreshRate
+            void setRefreshInterval(const float &refresh_interval);
 
-    /** Sets a maximum value for the PID integrator. */
-    void pid_set_integral_limit(float max);
+            /// Set the refresh frequency of the controller in hertz.
+            /// If you don't know the refresh frequency you can set the refresh interval using setRefreshRate
+            void setRefreshRate(const float &refresh_rate);
 
-    /** Resets the PID integrator to zero. */
-    void pid_reset_integral();
+            /// Set the minimun error for computation of the PID loop. The default is 0.0
+            /// It can be set to prevent integral windup or updates with minimal errors
+            void setErrorThreshold(const float &error_threshold);
 
-    /** Sets the PID frequency for gain compensation. */
-    void pid_set_frequency(float frequency);
+            /// Set the lower limit of the output. Output will be clamped to this value.
+            /// If clamped, the integral will not be computed to prevent integral windup.
+            /// To set the upper limit use setOutputUpperLimit
+            /// YOU NEED TO SET IT!
+            void setOutputLowerLimit(const float &output_lower_limit);
 
-    /** Gets the PID frequency for gain compensation. */
-    float pid_get_frequency();
+            /// Set the upper limit of the output. Output will be clamped to this value.
+            /// If clamped, the integral will not be computed to prevent integral windup.
+            /// To set the lower limit use setOutputLowerLimit
+            /// YOU NEED TO SET IT!
+            void setOutputUpperLimit(const float &output_upper_limit);
 
-private:
+            /// Set the desired point of the output.
+            /// This is the output the PID loop will try to reach.
+            /// error will be computed subtracting the feedback input from this value
+            void setDesiredPoint(const float &desired_point);
 
-    float kp_;
-    float ki_;
-    float kd_;
-    float integrator_;
-    float previous_error_;
-    float integrator_limit_;
-    float frequency_;
+            /// The refresh funcion that make all the PID computation.
+            /// Call it at specific rate, setted by setRefreshRate or setRefreshInterval (read descriptions).
+            /// (use a timer interrupt if used on a embedded microcontroller)
+            /// The feedback_input is the value from the feedback.
+            /// The returned value is the output value of the filter.
+            float refresh(const float &feedback_input);
 
 };
 
--- src/robot_controller/pid_controller.cpp
+++ src/robot_controller/pid_controller.cpp
@@ -5,81 +5,89 @@
 namespace controller
 {
 
-PID_controller::PID_controller()
-{
-   kp_ = 1;
-   ki_ = 1;
-   kd_ = 1;
-   integrator_ = 0;
-   previous_error_ = 0;
-   integrator_limit_ = INFINITY;
-   frequency_ = 1;
-}
+	PID_controller::PID_controller() :
+        last_error(0.f),
+        last_last_error(0.f),
+        last_output(0.f),
+        integral(0.f),
+        error_threshold(0.f)
+        {}
 
-void PID_controller::pid_set_gains(float kp, float ki, float kd)
-{
-   kp_ = kp;
-   ki_ = ki;
-   kd_ = kd;
-}
+	void PID_controller::setKp(const float &Kp) {
+		this->Kp = Kp;
+	}
 
-void PID_controller::pid_get_gains(float &kp, float &ki, float &kd)
-{
-    kp = kp_;
-    ki = ki_;
-    kd = kd_;
-}
+	void PID_controller::setKi(const float &Ki) {
+		this->Ki = Ki*interval;
+	}
 
-float PID_controller::pid_get_integral_limit()
-{
-    return integrator_limit_;
-}
+	void PID_controller::setKd(const float &Kd) {
+		this->Kd = Kd/interval;
+	}
 
-float PID_controller::pid_get_integral()
-{
-    return integrator_;
-}
-
-float PID_controller::pid_process(float error)
-{
-    float output;
-    integrator_ += error;
-
-    if (integrator_ > integrator_limit_) {
-
-        integrator_ = integrator_limit_;
-        
-    } else if (integrator_ < - integrator_limit_) {
-
-        integrator_ = - integrator_limit_;
+	void PID_controller::setWeights(const float &Kp, const float &Ki, const float &Kd) {
+		setKp(Kp);
+		setKi(Ki);
+		setKd(Kd);
     }
 
-    output  = - kp_ * error;
-    output += - ki_ * integrator_ / frequency_;
-    output += - kd_ * (error - previous_error_) * frequency_;
+	void PID_controller::setRefreshInterval(const float &refresh_interval) {
+		interval = refresh_interval;
+	}
 
-    previous_error_ = error;
-    return output;
-}
+	void PID_controller::setRefreshRate(const float &refresh_rate) {
+		interval = 1.f/refresh_rate;
+	}
 
-void PID_controller::pid_set_integral_limit(float max)
-{
-    integrator_limit_ = max;
-}
+	void PID_controller::setErrorThreshold(const float &error_threshold) {
+		this->error_threshold = error_threshold;
+	}
 
-void PID_controller::pid_reset_integral()
-{
-    integrator_ = 0.;
-}
+	void PID_controller::setOutputLowerLimit(const float &output_lower_limit) {
+		this->output_lower_limit = output_lower_limit;
+	}
 
-void PID_controller::pid_set_frequency(float frequency)
-{
-    frequency_ = frequency;
-}
+	void PID_controller::setOutputUpperLimit(const float &output_upper_limit) {
+		this->output_upper_limit = output_upper_limit;
+	}
 
-float PID_controller::pid_get_frequency()
-{
-    return frequency_;
-}
+	void PID_controller::setDesiredPoint(const float &desired_point) {
+		set_point = desired_point;
+	}
 
-}
+	float PID_controller::refresh(const float &feedback_input) {
+
+		error = set_point-feedback_input;
+		if (error >= error_threshold or error <= -error_threshold) {
+
+			// last_output = Kp*error + Ki*integral + Kd*(error-last_error);
+            last_output = Kp * error - Ki * last_error + Kd * last_last_error; 
+
+            last_last_error = last_error;
+            last_error = error;
+            
+			if (last_output > output_upper_limit) {
+
+				last_output = output_upper_limit;
+				// if (integral/error < 0.f) {
+				// 	integral += (error+last_error)/2.f;
+				// 	last_error = error;
+				// }
+				return output_upper_limit;
+			}
+			if (last_output < output_lower_limit) {
+
+				// if (integral/error < 0.f) {
+				// 	integral += (error+last_error)/2.f;
+				// 	last_error = error;
+				// }
+				last_output = output_lower_limit;
+				return output_lower_limit;
+			}
+			// integral += (error+last_error)/2.f;
+			// last_error = error;
+		}
+		return last_output;
+	}
+
+};
