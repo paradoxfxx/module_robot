cd9d7c87db4b02eb30015c42d8fe7ef85ffa6d26
Jiongyu <35024339@qq.com>
2019-11-05 22:05:24 +0800

    fix common.h

--- include/common.h
+++ include/common.h
@@ -1,8 +1,7 @@
 #ifndef COMMON_H
 #define COMMON_H
 
-namespace common
-{
+
 
 // Reduction ratio
 #define REDUCTION_RATIO 160
@@ -24,17 +23,44 @@ namespace common
 
 #define NETWORK_CARD_NAME "enp4s0"
 
-// Mode of operation(6061h)
-#define PROFILE_POSITION_MODE 1
-#define VELOCITY_MODE 2
-#define PROFILE_VELOCITY_MODE 3 
-#define TORQUE_PROFILE_MODE 4
-#define HOMING_MODE 6
-#define INTERPOLATED_POSITION_MODE 7
-#define CYCLIC_SYNCHRONOUS_POSITION_MODE 8
-#define CYCLIC_SYNCHRONOUS_VELOCITY_MODE 9
-#define CYCLIC_SYNCHRONOUS_TORQUE_MODE  10
+/*convert joint data(pos,vel())to motor(count) */
+#define DATA_TO_COUNT(DATA)             DATA * RESOLUTION * REDUCTION_RATIO / 360
 
-}
+/*convert motor data(cout) to joint data(pos,vel) */
+#define COUNT_TO_DATA(COUNT)            COUNT * 360 / RESOLUTION / REDUCTION_RATIO
+
+/*convert user torque(mN.m) to motor torque(per thousand of rate torque) */
+#define TORQUE_USER_TO_MOTOR(TORQUE)    TORQUE * 1000 / RATE_TORQUE
+
+/*convert motor torque(per thousand of rate torque) to user torque(mN.m) */
+#define TORQUE_MOTOR_TO_USER(TORQUE)    TORQUE * RATE_TORQUE / 1000
+
+
+
+typedef enum {NOT_READY, 
+              SWITCH_DISABLED,
+              READY_SWITCH, 
+              SWITCHED_ON,
+              OPERATION_ENABLED,
+              QUICK_STOP,
+              FAULT_REACTION,
+              FAULT,
+              UNKNOWN} PDS_STATUS;  // Statusword(6041h) 
+
+typedef enum {NO_MODE_CHANGE,
+              PROFILE_POSITION_MODE,
+              VELOCITY_MODE,
+              PROFILE_VELOCITY_MODE,
+              TORQUE_PROFILE_MODE,
+              HOMING_MODE,
+              INTERPOLATED_POSITION_MODE,
+              CYCLIC_SYNCHRONOUS_POSITION_MODE,
+              CYCLIC_SYNCHRONOUS_VELOCITY_MODE,
+              CYCLIC_SYNCHRONOUS_TORQUE_MODE} PDS_OPERATION; // Mode of operation(6061h) 
+
+ typedef enum {//HALT, FAULT_RESET, ENABLE_OPERATION, QUICK_STOP, ENABLE_VOLTAGE, SWITCH_ON, 
+} PDS_CONTROL; // Controlworld(6040h)
+
+
 
 #endif
