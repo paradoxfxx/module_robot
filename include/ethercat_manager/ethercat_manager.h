a57831724c28dd2c0edbac4a098b07ac258b7f44
Jiongyu <35024339@qq.com>
2019-11-05 22:06:06 +0800

    del some unrelate code

--- include/ethercat_manager/ethercat_manager.h
+++ include/ethercat_manager/ethercat_manager.h
@@ -1,30 +1,3 @@
-/*
- * Copyright (C) 2015, Jonathan Meyer
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *   * Redistributions of source code must retain the above copyright notice,
- *     this list of conditions and the following disclaimer.
- *   * Redistributions in binary form must reproduce the above copyright
- *     notice, this list of conditions and the following disclaimer in the
- *     documentation and/or other materials provided with the distribution.
- *   * Neither the names of Tokyo Opensource Robotics Kyokai Association. nor the names of its
- *     contributors may be used to endorse or promote products derived from
- *     this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
- */
-
 #ifndef ETHERCAT_MANAGER_H
 #define ETHERCAT_MANAGER_H
 
@@ -57,7 +30,7 @@ public:
     explicit EtherCatError(const std::string& what)
     : std::runtime_error(what)
     {}
-    };
+};
 
     /**
     * \brief This class provides a CPP interface to the SimpleOpenEthercatMaster library
@@ -139,23 +112,23 @@ public:
     */
     int getNumClients() const;
 
-    /**
-    * \brief init the DC time
-    */ 
-    void initDistributeClock();
+    // /**
+    // * \brief init the DC time
+    // */ 
+    // void initDistributeClock();
 
-    /**
-    * \brief wait for next period.
-    */ 
-    void waitnextperiod();
+    // /**
+    // * \brief wait for next period.
+    // */ 
+    // void waitnextperiod();
 
-    /**
-    * \brief update the DC time
-    */ 
-    void updateDistributeClock();
+    // /**
+    // * \brief update the DC time
+    // */ 
+    // void updateDistributeClock();
 
 
-    void sent_receive_eth_msg();
+    // void sent_receive_eth_msg();
 
 private:
 
@@ -195,4 +168,3 @@ private:
 }
 
 #endif
-
